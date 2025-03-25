10dc5eaf008df5fbae8775eac3ef36bfa0c6f24b
Aarti Khant <163381937+aarti11khant@users.noreply.github.com>
2025-03-25 12:52:08 +0530

    Add files via upload

---
+++ nav.56.php
@@ -0,0 +1,13 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="services.php">Services</a></li>
+        <li><a href="LE_61.php">Lab Exercise 61</a></li>
+        <li><a href="LE_62.php">Lab Exercise 62</a></li>
+        <li><a href="LE_63.php">Lab Exercise 63</a></li>
+        <li><a href="LE_64.php">Lab Exercise 64</a></li>
+        <li><a href="LE_65.php">Lab Exercise 65</a></li>
+    </ul>
+</nav>
