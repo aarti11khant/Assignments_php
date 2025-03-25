3bb0a1567db98f1eb66cbea60a8a789a3697cc69
Aarti Khant <163381937+aarti11khant@users.noreply.github.com>
2025-03-25 12:22:08 +0530

    Add files via upload

---
+++ M1_LE_04.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Module 1 Lab Exercise 04</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+
+<body>
+
+    <div class="container-fluid">
+
+        <div class="row bg-dark text-white text-center">
+            <h1>Module 1 Lab Exercise 04</h1>
+        </div>
+
+        <div class="row bg-dark text-white text-center">
+            <p>Create multiple tables and perform queries using : SELECT, UPDATE, DELETE, INSERT, WHERE, LIKE, GROUP BY,
+                HAVING, LIMIT, OFFSET, Subqueries, AND, OR, NOT.</p>
+        </div>
+
+        <!-- Create multiple tables and perform queries using : SELECT, UPDATE, DELETE, INSERT, WHERE, LIKE, GROUP BY, HAVING, LIMIT, OFFSET, Subqueries, AND, OR, NOT -->
+
+        <h3>SQL Queries</h3>
+
+        <textarea id="text-input" rows="50" cols="100"
+            value=""><?php echo file_get_contents('M1_LE_04.md') ?></textarea>
+
+        <div id="preview"> </div>
+
+    </div>
+
+</body>
+
+</html>
